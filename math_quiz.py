c401da87214641ee5df3b21b110b16df50672e00
Axe90 <77531127+Axe90@users.noreply.github.com>
2021-03-22 20:27:15 +1100

    Update math_quiz.py
    filled out sum function and refined easy quiz

--- math_quiz.py
+++ math_quiz.py
@@ -1,11 +1,21 @@
 import random
 
-name = input(str("What is your name?:" ))
+name = input(str("What is your name?: " ))
+dif_in = input("Type 'e' for easy: ")
 
 
 
-def sum(num1, num2):
-    return num1 + num2
+def sum_easy():
+    a = random.randint(0, 10)
+    b = random.randint(0, 5)
+    answer = a + b
+    response = input("{a} + {b} = ".format(a = a, b = b))
+    if response == str(answer):
+        #correct = True
+        print("Correct!")
+    else:
+        print("Incorrect! the answer is: " + str(answer))
+    return 
 
 def sub(num1, num2):
     return num1 - num2
@@ -16,32 +26,26 @@ def mult(num1, num2):
 def div(num1, num2):
     return num1 / num2
 
-def dif(name = 'zac'):
-    if name == 'zac':
-        dif = "easy"
-        return dif
-    elif name == 'dom':
-        dif = 'hard'
-        return dif
+def dif(difficulty):
+    if difficulty == 'e':
+        dif_set = "easy"
+        return dif_set
+    elif difficulty == 'h':
+        dif_set = 'hard'
+        return dif_set
 
 def easy_quiz():
     score = 0
-    difficulty = dif(name)
+    difficulty = dif(dif_in)
     if difficulty == 'easy':
-        print("Welcome to the EASY quiz!")
-        a = random.randint(0, 10)
-        b = random.randint(0, 10)
-        #print("{a} + {b} = ".format(a = a, b = b))
-        answer = input("{a} + {b} = ".format(a = a, b = b))
-        if answer == str(sum(a, b)):
-            score += 1
-            print("Correct!")
-        else:
-            print("Incorrect! the answer is: " + str(sum(a, b)))
+        print("Welcome {0} to the EASY quiz!".format(name))
+        sum_easy()
+        sum_easy()
+        #print(score)  
 
 
 print(easy_quiz())
 #print(dif(name))
-    
+ 
 
 
