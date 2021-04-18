aa66201375db7b5b62b1a595155c5227e1f27fa7
Axe90 <77531127+Axe90@users.noreply.github.com>
2021-04-18 15:14:34 +1000

    Update math_quiz.py
    add medium quiz

--- math_quiz.py
+++ math_quiz.py
@@ -3,7 +3,7 @@ import random
 name = input(str("What is your name?: " ))
 while name == "":
     name = input(str("What is your name?: " ))
-dif_in = input("Type 'e' for EASY or 'h' for HARD: ")
+dif_in = input("Type 'e' for EASY or 'm' for MEDIUM or 'h' for HARD: ")
 
 
 
@@ -21,6 +21,32 @@ def sum_easy():
     return score
 
 def sub_easy():
+    a = random.randint(5, 10)
+    b = random.randint(0, 5)
+    answer = a - b
+    response = input("{a} - {b} = ".format(a = a, b = b))
+    if response == str(answer):
+        score = 1
+        print("Correct!"'\n')
+    else:
+        score = 0
+        print("Incorrect! the answer is: " + str(answer)+'\n')
+    return score  
+
+def sum_medium():
+    a = random.randint(0, 10)
+    b = random.randint(5, 10)
+    answer = a + b
+    response = input("{a} + {b} = ".format(a = a, b = b))
+    if response == str(answer):
+        score = 1
+        print("Correct!"'\n')
+    else:
+        score = 0
+        print("Incorrect! the answer is: " + str(answer)+'\n')
+    return score   
+
+def sub_medium():
     a = random.randint(5, 10)
     b = random.randint(0, 5)
     answer = a - b
@@ -76,11 +102,14 @@ def dif(difficulty):
     if difficulty == 'e':
         dif_set = "easy"
         return dif_set
+    elif difficulty == 'm':
+        dif_set = 'medium'
+        return dif_set
     elif difficulty == 'h':
         dif_set = 'hard'
         return dif_set
-    elif difficulty != 'e' or difficulty != 'h':
-        return dif(input("TRY AGAIN Type 'e' for EASY or 'h' for HARD: ")) 
+    elif difficulty != 'e' or difficulty != 'm' or difficulty != 'h':
+        return dif(input("TRY AGAIN Type 'e' for EASY or 'm' for MEDIUM or 'h' for HARD: ")) 
 
 
 def quiz():
@@ -95,6 +124,15 @@ def quiz():
         score += sum_easy()
         print("You finished the quiz with a score of " + str(score) + " out of 5 \n") 
         return "Thank you for playing\n" 
+    elif difficulty == 'medium':
+        print('\n'+"Welcome {0} to the MEDIUM quiz!".format(name)+'\n')
+        score += sum_medium()
+        score += sum_medium()
+        score += sub_medium()
+        score += sub_medium()
+        score += sum_medium()
+        print("You finished the quiz with a score of " + str(score) + " out of 5 \n") 
+        return "Thank you for playing\n"
     elif difficulty == 'hard':
         print('\n'+"Welcome {0} to the HARD quiz!".format(name)+'\n')
         score += mult_hard()
